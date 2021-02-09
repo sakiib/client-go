cc79f1f98286c267c61d05383b9534a74f9fc7a0
sakibalamin <sakibalamin@appscode.com>
2021-02-09 09:44:28 +0600

    cli init

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+  "fmt"
+  "github.com/spf13/cobra"
+  "os"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+  Use:   "root",
+  Short: "A brief description -> rootCmd",
+  Long: `A longer description -> rootCmd`,
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("root called!")
+  },
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+}
